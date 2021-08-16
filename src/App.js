b3197c06f9a06f98c7f24ba4883984d40d7ebb81
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-08-16 21:38:52 +0530

    created nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {makeStyles} from "@material-ui/core";
+import NavBar from "./components/NavBar/navBar";
+import Home from "./pages/home/home";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    // backgroundColor: "blueviolet",
+    minHeight: "100vh",
+
+  },
+  navBar:{
+    // backgroundColor:"#a1009f",
+    height: "15vh",
+  },
+  body:{
+    // backgroundColor:"#a1009f",
+  }
+}));
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={classes.root}
+          // initial={{opacity:0}}
+          // animate={{opacity:1}}
+          // transition={{duration: 1}}
+      >
+
+        <div className={classes.navBar}>
+          <NavBar/>
+        </div>
+        <div className={classes.body}>
+          <Home/>
+        </div>
+        <div className={classes.navBar}>
+
+        </div>
+      </div>
+
   );
 }
 
---
+++ src/pages/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from "../../components/Card/card";
+
+function Home(props) {
+    return (
+        <div>
+            test
+        </div>
+        // <Card/>
+    );
+}
+
+export default Home;
